047f934b0011ca207bc2c1c384e6caf389a867c0
230-21437 <23021437@vnu.edu.vn>
2025-03-07 20:30:57 +0700

    Update bài1.cpp

--- Lect3_Assignment/bài1.cpp
+++ Lect3_Assignment/bài1.cpp
@@ -1,15 +1,15 @@
-/*
-    Biến đổi:
-        4nlogn + 2n -> O(nlogn)
-        2^10        -> O(1)
-        3n+100logn  -> O(n)
-        2^logn      -> O(n)
-        4n          -> O(n)
-        n^2 + 10n   -> O(n^2)
-        2^n         -> O(2^n)
-        n^3         -> O(n^3)
-        nlogn       -> O(nlogn)
-    Sắp xếp:
+/ 
+*Biến đổi:
+    4nlogn + 2n -> O(nlogn)
+    2^10        -> O(1)
+    n+100logn  -> O(n)
+    2^logn      -> O(n)
+    4n          -> O(n)
+    n^2 + 10n   -> O(n^2)
+    2^n         -> O(2^n)
+    n^3         -> O(n^3)
+    nlogn       -> O(nlogn)
+Sắp xếp:
         2^10
         2^logn
         4n
